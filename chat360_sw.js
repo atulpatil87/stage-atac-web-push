1416d9fbfbdd34b36a5b0626a63b7f49a651d672
Atul Patil <atulp441@gmail.com>
2026-04-17 16:17:27 +0530

    update

--- chat360_sw.js
+++ chat360_sw.js
@@ -1,1 +1,5 @@
-var chat360Config = 
+var chat360Config = {
+  channelId: "79c2ee9d-7765-4475-9525-e0398db03082",
+  key: "BCITuDIhAVfw-GR2W8xto9IULNjG3OwORjLh5nm44Jw7n0jb1OxF9LhPebTJNbEG-VRQJw_fGTVTyKrbKPxG4mk"
+};
+importScripts("https://staging.chat360.io/push/service_worker/sw.js");
